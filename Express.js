83c7a66e27d2e1c5a9a5e186c035dd9553c2fc5d
GLBryan <garrettleebryan@gmail.com>
2021-11-05 12:56:00 -0400

    landing page get route

--- Express.js
+++ Express.js
@@ -6,6 +6,11 @@ const PORT = 3001
 
 app.use(express.static('public'))
 
+// Homepage 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(PORT, () => {
     console.log("Listening at port 3001!")
 })
