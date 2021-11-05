e3f3e7d8b2c7e071c34649bc3e4376b1efed1d9b
GLBryan <garrettleebryan@gmail.com>
2021-11-05 17:02:18 -0400

    post and get routes adjusted

--- Express.js
+++ Express.js
@@ -12,11 +12,6 @@ app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// * Homepage *
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
 // * Notes page *
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
@@ -24,16 +19,23 @@ app.get('/notes', (req, res) => {
 
 // * Get route for fetching notes *
 app.get('/api/notes', (req, res) => {
-    res.json(db)
+    console.log(`A ${req.method} request was received`)
+    
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+            const parse = JSON.parse(data)
+            res.json(parse)
+        }
+    })
 })
 
 // * Post route for new notes
 app.post('/api/notes', (req, res) => {
     console.log(`A ${req.method} request was received`)
 
-    console.log(req.body)
     const {title, text} = req.body
-
     const newNote = {
         title,
         text,
@@ -43,9 +45,7 @@ app.post('/api/notes', (req, res) => {
         if (err) {
           console.error(err);
         } else {
-          
           const existingNotes = JSON.parse(data);
-  
           existingNotes.push(newNote)
   
           fs.writeFile('./db/db.json', JSON.stringify(existingNotes, null, 2), (error) => {
@@ -58,6 +58,11 @@ app.post('/api/notes', (req, res) => {
             });
         }
     }); 
+})
+
+// * Homepage *
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.listen(PORT, () => {
