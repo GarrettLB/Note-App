10794a70fb5668e210598267ebd73fabbe9cf088
GLBryan <garrettleebryan@gmail.com>
2021-11-05 14:07:37 -0400

    added middleware

--- Express.js
+++ Express.js
@@ -7,6 +7,10 @@ const PORT = 3001
 
 app.use(express.static('public'))
 
+// * Middleware *
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // * Homepage *
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -17,8 +21,18 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
+// * Route for fetching notes *
 app.get('/api/notes', (req, res) => {
     res.json(db)
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(`A ${req.method} request was received`)
+
+    console.log(req.body)
+    const {title, text} = req.body
+
+    console.log(title, text)
 })
 
 app.listen(PORT, () => {
